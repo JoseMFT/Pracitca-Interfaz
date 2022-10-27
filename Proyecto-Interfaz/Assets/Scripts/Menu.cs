5ffdd4cef53de986d555ec63bb4e55c751bf0850
JoseMFT <jomaft03@gmail.com>
2022-10-27 12:12:59 +0100

    27/10/2022 - 12:12
    Menú de ajustes por pulir

--- Proyecto-Interfaz/Assets/Scripts/Menu.cs
+++ Proyecto-Interfaz/Assets/Scripts/Menu.cs
@@ -5,31 +5,23 @@ using UnityEngine;
 public class Menu: MonoBehaviour {
 
     [SerializeField]
-    GameObject PlayerUI, Player, CanvasMenu;
-    void Start () {
-
+    GameObject canvasMenu, canvasInicio;
+    public void clicked () {
+        canvasMenu.SetActive (true);
+        LeanTween.alphaCanvas (canvasMenu.GetComponent<CanvasGroup> (), 0.0f, 0.0f);
+        LeanTween.scale (canvasMenu, new Vector3 (0.0f, 0.0f, 0.0f), 0.0f).setOnComplete (aparecer);
     }
 
-    void activado () {
-        Player.SetActive (false);
-        PlayerUI.SetActive (false);
-        if (CanvasMenu.activeSelf) {
-            CanvasMenu.SetActive (false);
-        } else {
-            CanvasMenu.SetActive (true);
-        }
+    void aparecer () {
+        canvasInicio.SetActive (false);
+        LeanTween.scale (canvasMenu, new Vector3 (1f, 1f, 1f), 0.5f);
+        LeanTween.alphaCanvas (canvasMenu.GetComponent<CanvasGroup> (), 1f, 0.35f);
     }
 
-    // Update is called once per frame
-    void Update () {
-        if (Input.GetKey ("esc")) {
-            Player.SetActive (false);
-            PlayerUI.SetActive (false);
-            if (CanvasMenu.activeSelf) {
-                CanvasMenu.SetActive (false);
-            } else {
-                CanvasMenu.SetActive (true);
-            }
-        }
+    public void cerrar () {
+        canvasInicio.SetActive (true);
+        LeanTween.scale (canvasMenu, new Vector3 (0f, 0f, 0f), 0.35f);
+        LeanTween.alphaCanvas (canvasMenu.GetComponent<CanvasGroup> (), 0f, 0.5f);
+        canvasMenu.SetActive (false);
     }
 }
